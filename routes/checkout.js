1cf0295932cfa99ec3707d7aa87294782e5dfc97
Kate Jewett-Williams <36312087+kjdub01@users.noreply.github.com>
2024-11-06 23:38:23 +0000

    do drop-in ui turtorial

---
+++ routes/checkout.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: '<nzndht9r7bk4758v>',
+    publicKey: '<yrmdr6458bz4bqt5>',
+    privateKey: '<13590f21863126b4085123961bc31bc3>'
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
